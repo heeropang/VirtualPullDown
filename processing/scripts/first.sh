72b30548a91ab70c0723be8de895000045feabeb
Heewhan S <hshin40@gmail.com>
2023-04-27 12:46:21 -0500

    Delete processing directory

--- processing/scripts/first.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "setting up directories for $1 integrase"
-echo "copy and paste the following line for rsync"
-echo "rsync -auvz * heewhan@midway3.rcc.uchicago.edu:/beagle3/price/top_search/$1/ready"
-mkdir $1
-cd $1
-mkdir ready msas predictions log
