6160cf2a32ee7e9e5e1e0a46424a479ad897821a
Heewhan S <hshin40@gmail.com>
2023-05-08 06:17:27 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="virtualpulldown",
+    version="0.0.1",
+    author="Heewhan Shin",
+    author_email="hshin40@gmail.com",
+    description="A package for virtual pulldown",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/heeropang/VirtualPullDown",
+    keywords = ['PDB', 'structure biology', 'protein', 'analysis', 'ColabFold', 'AlphaFold'],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
