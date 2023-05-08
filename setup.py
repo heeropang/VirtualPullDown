871a8a5a66962126f35d31ddae43aece38285a7b
Heewhan S <hshin40@gmail.com>
2023-05-08 06:30:10 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="A package for virtual pulldown",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/heeropang/VirtualPullDown",
+    url="https://github.com/heeropang/VirtualPullDown/scripts",
     keywords = ['PDB', 'structure biology', 'protein', 'analysis', 'ColabFold', 'AlphaFold'],
     packages=setuptools.find_packages(),
     classifiers=[
